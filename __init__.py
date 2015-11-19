8996c17ef3703dce8ad54c3b9ca24673bedd8818
Peter Petrik <zilolv@gmail.com>
2015-11-19 12:03:14 +0100

    add support for more bands (RANGEITEM)

--- __init__.py
+++ __init__.py
@@ -17,54 +17,62 @@ import collections
 def classFactory(iface):
     return MinimalPlugin(iface)
 
-def export_to_mapserver_format(cr_name, min_val, max_val, nodata_val, invert):
-    cr = QgsStyleV2.defaultStyle().colorRamp(cr_name)
-    if not cr:
-        return "Invalid colormap name"
+class Exporter():
+    def __init__(self, cr_name, riattr, min_val, max_val, nodata_val, invert):
+        self.cr_name = cr_name
+        self.riattr = riattr
+        self.min_val = min_val
+        self.max_val = max_val
+        self.nodata_val = nodata_val
+        self.invert = invert
 
-    ramp = collections.OrderedDict()
-    ramp[0] = cr.color1()
-    for cstop in cr.stops():
-        ramp[cstop.offset] = cstop.color
-    ramp[1] = cr.color2()
-    return ramp_to_style(cr_name, min_val, max_val, nodata_val, invert, ramp)
+    def export(self):
+        cr = QgsStyleV2.defaultStyle().colorRamp(self.cr_name)
+        if not cr:
+            return "Invalid colormap name"
 
-def color_to_rgb(color):
-    return str(color.red()) + " " + str(color.green()) + " " + str(color.blue())
+        ramp = collections.OrderedDict()
+        ramp[0] = cr.color1()
+        for cstop in cr.stops():
+            ramp[cstop.offset] = cstop.color
+        ramp[1] = cr.color2()
+        return self._ramp_to_style(ramp)
 
-def ramp_to_style(cr_name, min_val, max_val, nodata_val, invert, ramp):
-    res = "# From QGIS colormap: " + cr_name + "\n"
-    if invert:
-        res += "# Inverted\n"
-    res += "# Min value: " + str(min_val) + "\n"
-    res += "# Max value: " + str(max_val) + "\n"
-    res += "# NODATA value: " + str(nodata_val) + "\n"
-    last_color = None
-    last_value = None
+    def _color_to_rgb(self, color):
+        return str(color.red()) + " " + str(color.green()) + " " + str(color.blue())
 
-    for perc, color in ramp.iteritems():
-        value = min_val + (max_val - min_val)*perc
+    def _ramp_to_style(self, ramp):
+        res = "# From QGIS colormap: " + self.cr_name + "\n"
+        if self.invert:
+            res += "# Inverted\n"
+        res += "# Min value: " + str(self.min_val) + "\n"
+        res += "# Max value: " + str(self.max_val) + "\n"
+        res += "# NODATA value: " + str(self.nodata_val) + "\n"
+        res += "CLASS\n"
+        last_color = None
+        last_value = None
 
-        if invert:
-            color = QColor(256-color.red(), 256-color.green(), 256-color.blue())
+        for perc, color in ramp.iteritems():
+            value = self.min_val + (self.max_val - self.min_val)*perc
 
-        if last_color is not None:
-            res += "CLASS\n"
+            if self.invert:
+                color = QColor(256-color.red(), 256-color.green(), 256-color.blue())
 
-            if last_value < nodata_val and value > nodata_val:
-                res += "  EXPRESSION (([pixel] < " + str(nodata_val) + ") OR ([pixel] > " + str(nodata_val+0.001) + "))\n"
+            if last_color is not None:
+                if last_value < self.nodata_val and value > self.nodata_val:
+                    res += "  EXPRESSION (([" + self.riattr +"] < " + str(self.nodata_val) + ") OR ([" + self.riattr +"] > " + str(self.nodata_val+0.001) + "))\n"
 
-            res += "  STYLE\n"
-            res += "    DATARANGE " + str(last_value) + " " + str(value) + "\n"
-            res += "    RANGEITEM \"pixel\"\n"
-            res += "    COLORRANGE " + color_to_rgb(last_color) + " " + color_to_rgb(color) + "\n"
-            res += "  END\n"
-            res += "END\n"
+                res += "  STYLE\n"
+                res += "    DATARANGE " + str(last_value) + " " + str(value) + "\n"
+                res += "    RANGEITEM \"" + self.riattr +"\"\n"
+                res += "    COLORRANGE " + self._color_to_rgb(last_color) + " " + self._color_to_rgb(color) + "\n"
+                res += "  END\n"
 
-        last_color = color
-        last_value = value
+            last_color = color
+            last_value = value
 
-    return res
+        res += "END\n"
+        return res
 
 class PluginDialog(QDialog):
     def __init__(self):
@@ -74,13 +82,15 @@ class PluginDialog(QDialog):
         vbox.addStretch(1)
 
         lName = QLabel("Colormap name")
-        self.tbName = QLineEdit()
+        self.tbName = QLineEdit("Greens")
         lMin = QLabel("Minimum value")
-        self.tbMin = QLineEdit()
+        self.tbMin = QLineEdit("0.0")
         lMax = QLabel("Maximum value")
-        self.tbMax = QLineEdit()
+        self.tbMax = QLineEdit("1.0")
         lNoData = QLabel("NODATA value")
         self.tbNoData = QLineEdit("0.0")
+        lRIAttr= QLabel("RANGEITEM attribute")
+        self.tbRIAttr = QLineEdit("pixel")
         self.cbInverted=QCheckBox("Inverted?")
         okButton = QPushButton("OK")
 
@@ -92,6 +102,8 @@ class PluginDialog(QDialog):
         vbox.addWidget(self.tbMax)
         vbox.addWidget(lNoData)
         vbox.addWidget(self.tbNoData)
+        vbox.addWidget(lRIAttr)
+        vbox.addWidget(self.tbRIAttr)
         vbox.addWidget(self.cbInverted)
         vbox.addWidget(okButton)
 
@@ -106,7 +118,10 @@ class PluginDialog(QDialog):
             max_val = float(self.tbMax.text());
             nodata_val = float(self.tbNoData.text());
             invert = self.cbInverted.isChecked();
-            res = export_to_mapserver_format(cr_name, min_val, max_val, nodata_val, invert)
+            riattr = self.tbRIAttr.text();
+            exporter = Exporter(cr_name, riattr, min_val, max_val, nodata_val, invert)
+            res = exporter.export()
+
         except ValueError:
             res = "Invalid min/max (not float)"
 
